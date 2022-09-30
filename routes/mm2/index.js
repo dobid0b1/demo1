5db35ba9d8008bb3bef7a276171812c1c5add511
d0b1.q333 <d0b1.q333@gmail.com>
2022-09-30 21:55:12 +0700

    ..

---
+++ routes/mm2/index.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+
+// ส่งตัวแปรแบบ ?
+router.get('/', function (req, res) {
+    const {dd} = req.query
+    res.send('m2')
+})
+
+// ส่งตัวแปรแบบฟังก์ชั่น
+router.get('/:id', (req, res) => {
+    const {id, name} = req.params
+    res.send(`id : ${id} ++ name : ${name}`)
+})
+
+router.post('/', (req, res) => {
+    const { name } = req.body
+
+    // if(name == 'b1') {
+    //     res.status(200).json({
+    //         status: true,
+    //         message: `Success`,
+    //         name
+    //     })
+    // }
+    // else {
+    //     res.status(500).json({
+    //         status: false,
+    //         message: `error`,
+    //     })
+    // }
+
+    if(!name) return res.status(500).json({
+        staus: false,
+        msg: 'ไม่มีชื่อ'
+    })
+
+    if (name !== 'dobi') return res.status(500).json({
+        staus: false,
+        msg: 'Error'
+    })
+
+    res.status(200).json({
+        status: true,
+        msg: 'success'
+    })
+
+    
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+const port = process.env.PORT
+app.use(express.json());
+
+const routes = require("./routes/mm1")
+app.use(routes)
+
+const routes2 = require("./routes/mm2")
+app.use(routes2)
+
+app.listen(port, () => {
+    console.log(`Start port ${port}`)
+})
